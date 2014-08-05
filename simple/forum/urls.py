c9ccfe07c94edf94f5c0390e42584778817554e7
aliyarahman <aliya.rahman@yahoo.com>
2014-08-05 10:34:15 -0400

    Fixed file naming for consistency

--- simple/forum/urls.py
+++ simple/forum/urls.py
@@ -6,8 +6,10 @@ from forum import views
 urlpatterns = patterns('',
 
 	url(r'^index/$', views.index, name="index"),
+	url(r'^signUp/$', views.signUp, name="signUp"),
 	url(r'^forumHome/$', views.forumHome, name="forumHome"),
 	url(r'^forumView/(?P<forum_id>\d+)/$', views.forumView, name="forumView"),
 	url(r'^threadView/(?P<thread_id>\d+)/$', views.threadView, name="threadView"),
+	url(r'^startThread/$', views.startThread, name="startThread"),
 	url(r'^editProfile/(?P<user_id>\d+)/$', views.editProfile, name="editProfile"),
 	)	
--- simple/forum/views.py
+++ simple/forum/views.py
@@ -9,21 +9,27 @@ from django.template.loader import get_template
 # Create your views here.
 
 def index(request):
-	return render(request, "Index.html",)
+	return render(request, "index.html",)
 
 def forumHome(request):
 	all_forums = Forum.objects.all()
-	return render(request, 'Forums.html', {'all_forums': all_forums})
+	return render(request, 'forums.html', {'all_forums': all_forums})
 
 def forumView(request,forum_id):
 	forum = get_object_or_404(Forum, pk=forum_id)
 	all_threads = Thread.objects.filter(forum=forum).all()
-	return render(request, 'ForumView.html', {'forum': forum, 'all_threads': all_threads})
+	return render(request, 'forumView.html', {'forum': forum, 'all_threads': all_threads})
 
 def threadView(request,thread_id):
 	thread = get_object_or_404(Thread, pk=thread_id)
 	all_posts = Post.objects.filter(thread=thread).all()
-	return render(request, 'ThreadView.html', {'thread': thread, 'all_posts': all_posts})
+	return render(request, 'threadView.html', {'thread': thread, 'all_posts': all_posts})
+
+def startThread(request):
+	return render(request, 'startThread.html')
+
+def signUp(request):
+	return render(request, 'signUp.html')
 
 def editProfile(request,user_id):
 	profile = get_object_or_404(UserProfile, pk=user_id)
